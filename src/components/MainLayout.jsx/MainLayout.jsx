5083b74e68ce11a67367bda76ad7f209af5c4714
Joey Kuo <joeyskuo@gmail.com>
2026-01-12 18:34:37 -0800

    MainLayout id and styling

--- src/components/MainLayout.jsx/MainLayout.jsx
+++ src/components/MainLayout.jsx/MainLayout.jsx
@@ -1,16 +1,17 @@
 import CameraViewPlayer from "../CameraViewPlayer/CameraViewPlayer";
 import ImageProcessingContainer from "../ImageProcessingContainer/ImageProcessingContainer";
 import WorldViewPlayer from "../WorldViewPlayer/WorldViewPlayer";
+import './MainLayout.css';
 
 const MainLayout = () => {
     return (
-        <>
+        <div id="main-layout">
             <WorldViewPlayer/>
             <div>
                 <CameraViewPlayer/>
                 <ImageProcessingContainer/>
             </div>
-        </>
+        </div>
     )
 };
 
