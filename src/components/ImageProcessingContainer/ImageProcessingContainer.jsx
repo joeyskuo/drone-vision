7cd97d6439624e926dae6f034734c418533ae24d
Joey Kuo <joeyskuo@gmail.com>
2026-01-15 20:40:45 -0800

    frame capture button and function created

--- src/components/ImageProcessingContainer/ImageProcessingContainer.jsx
+++ src/components/ImageProcessingContainer/ImageProcessingContainer.jsx
@@ -1,6 +1,35 @@
 const ImageProcessingContainer = () => {
+    
+    function captureFrame() {
+        const CameraViewVideo = document.querySelector('.camera-view-player video');
+
+        // create canvas matching video size
+        const frameCanvas = document.createElement('canvas');
+        frameCanvas.width = CameraViewVideo.videoWidth;
+        frameCanvas.height = CameraViewVideo.videoHeight;
+        
+        // capture frame
+        const ctx = frameCanvas.getContext('2d');
+        ctx.drawImage(CameraViewVideo, 0, 0, frameCanvas.width, frameCanvas.height);
+        
+        // Convert to image and display
+        const frameImage = document.createElement('img');
+        frameImage.src = frameCanvas.toDataURL('image/jpeg', 0.95);
+        frameImage.style.width = '100%';
+        
+        // Clear container and add image
+        const container = document.querySelector('.captured-frame-container');
+        container.innerHTML = '';
+        container.appendChild(frameImage);
+    }
+
     return (
-        <div>ImageProcessingContainer</div>
+        <div>
+            <div>ImageProcessingContainer</div>
+            <div>Captured Image Here</div>
+            <div className='captured-frame-container'></div>
+            <button onClick={captureFrame}>Capture</button>
+        </div>
     )
 };
 
