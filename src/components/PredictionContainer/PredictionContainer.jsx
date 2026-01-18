3149ee006318038312e3750fdc3bb69637d5d83c
Joey Kuo <joeyskuo@gmail.com>
2026-01-17 16:27:52 -0800

    PredictionContainer reads from context

--- src/components/PredictionContainer/PredictionContainer.jsx
+++ src/components/PredictionContainer/PredictionContainer.jsx
@@ -1,6 +1,24 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+
 const PredictionContainer = () => {
+
+    const { appState } = useContext(AppContext);
+    
+    console.log(appState);
+
     return (
-        <div>PredictionContainer</div>
+        <div>
+            <h3>Predictions</h3>
+            <div>
+                {appState?.probabilities?.map((prob, index) => (
+                    <div key={index}>
+                        <p><strong>Object:</strong> {prob.object}</p>
+                        <p><strong>Confidence:</strong> {prob.confidence}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
