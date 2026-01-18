592c65a76b2c004db25cab63ed206b6038886f66
Joey Kuo <joeyskuo@gmail.com>
2026-01-17 16:03:20 -0800

    WorldView figcaption

--- src/components/WorldViewPlayer/WorldViewPlayer.jsx
+++ src/components/WorldViewPlayer/WorldViewPlayer.jsx
@@ -1,6 +1,7 @@
 const WorldViewPlayer = () => {
     return (
         <figure className="world-view-player">
+            <figcaption>World View</figcaption>
             <video controls width="100%">
                 <source src="/videos/world.mp4" type="video/mp4" />
             </video>
