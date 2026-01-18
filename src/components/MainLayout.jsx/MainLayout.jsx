0027f7797d5773b95f9584bb0c8c8ee675f574ab
Joey Kuo <joeyskuo@gmail.com>
2026-01-17 16:04:03 -0800

    MainLayout split into three sections

--- src/components/MainLayout.jsx/MainLayout.jsx
+++ src/components/MainLayout.jsx/MainLayout.jsx
@@ -1,16 +1,21 @@
 import CameraViewPlayer from "../CameraViewPlayer/CameraViewPlayer";
 import ImageProcessingContainer from "../ImageProcessingContainer/ImageProcessingContainer";
+import PredictionContainer from "../PredictionContainer/PredictionContainer";
 import WorldViewPlayer from "../WorldViewPlayer/WorldViewPlayer";
 import './MainLayout.css';
 
 const MainLayout = () => {
     return (
         <div id="main-layout">
+            <div>
             <WorldViewPlayer/>
+            <div>Description Here</div>
+            </div>
             <div>
                 <CameraViewPlayer/>
                 <ImageProcessingContainer/>
             </div>
+            <PredictionContainer/>
         </div>
     )
 };
