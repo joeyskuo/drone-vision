21b681dda11a40c8c75b95444566c3b16b592397
Joey Kuo <joeyskuo@gmail.com>
2026-01-19 18:20:17 -0800

    restructured MainLayout

--- src/components/MainLayout.jsx/MainLayout.jsx
+++ src/components/MainLayout.jsx/MainLayout.jsx
@@ -2,20 +2,28 @@ import CameraViewPlayer from "../CameraViewPlayer/CameraViewPlayer";
 import ImageProcessingContainer from "../ImageProcessingContainer/ImageProcessingContainer";
 import PredictionContainer from "../PredictionContainer/PredictionContainer";
 import WorldViewPlayer from "../WorldViewPlayer/WorldViewPlayer";
-import './MainLayout.css';
+import './MainLayout.scss';
 
 const MainLayout = () => {
     return (
         <div id="main-layout">
-            <div>
-            <WorldViewPlayer/>
-            <div>Description Here</div>
-            </div>
-            <div>
+            <section className="title-section">
+                <div>
+                    <h1>Drone Vision</h1>
+                    <p>Simulation and Image Processing</p>
+                </div>
+            </section>
+            <section className="video-section">
                 <CameraViewPlayer/>
+                <WorldViewPlayer/>
+            </section>
+            <section className="image-process-section">
                 <ImageProcessingContainer/>
-            </div>
-            <PredictionContainer/>
+            </section>
+            <section>
+                <div>ROS2 &middot; Gazebo &middot; TensorFlow</div>
+                <div>Robot created as URDF</div>
+            </section>
         </div>
     )
 };
