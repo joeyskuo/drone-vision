87abf9910ec0458e09703efd6cb79d5f7d2b8658
Joey Kuo <joeyskuo@gmail.com>
2026-01-23 17:07:17 -0800

    about-section rename; descriptions and sections updated

--- src/components/MainLayout.jsx/MainLayout.jsx
+++ src/components/MainLayout.jsx/MainLayout.jsx
@@ -19,21 +19,22 @@ const MainLayout = () => {
             <section className="image-process-section">
                 <ImageProcessingContainer/>
             </section>
-            <section className="repo-section">
-                <div>
-                    <div>Project Description</div>
+            <section className="about-section">
+                <section className="project-description-section">
+                    <h3>Project Description</h3>
+                    <p>A drone is operated in a simulated world, and images from its camera feed are run through a pre-trained object detection model</p>
                     <div>
-                        <div>Pending</div>
+                        <div><strong>Pending</strong></div>
                         <ul>
                             <li>aerial drone model and movement</li>
                             <li>custom machine learning model</li>
                         </ul>
                     </div>
-                </div>
-                <div>
+                </section>
+                <section className="repo-section">
                     <h3>Modules</h3>
                     <article>
-                        <div>ROS2 Drone Workspace</div>
+                        <div>ROS2 Drone Workspace (Gazebo, RViz, TF, Xacro)</div>
                         <a href="http://github.com">Repo</a>
                     </article>
                     <article>
@@ -41,10 +42,10 @@ const MainLayout = () => {
                         <a href="http://github.com">Repo</a>
                     </article>
                     <article>
-                        <div>Web App</div>
+                        <div>Web App (React, Tensorflow, Vite, SCSS)</div>
                         <a href="http://github.com">Repo</a>
                     </article>
-                </div>
+                </section>
             </section>
         </div>
     )
