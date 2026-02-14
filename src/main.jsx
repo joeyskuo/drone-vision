6c2909ebca75b42794336193db6c9fc802799129
Joey Kuo <joeyskuo@gmail.com>
2026-02-13 21:09:35 -0800

    import instrument; sentry uses react error hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
+import './instrument'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from "@sentry/react"
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement, {
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  onCaughtError: Sentry.reactErrorHandler(),
+  onRecoverableError: Sentry.reactErrorHandler(),
+});
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
